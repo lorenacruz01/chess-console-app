ed9cb3fb9130ad77d7f4d898b5ed1caa2178eebc
lorenacruz01 <68523064+lorenacruz01@users.noreply.github.com>
2022-02-01 13:05:43 -0300

    Criação da classe Position

---
+++ chess-console-app/chess-console-app/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Board;
+
+namespace chess_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //testing position class
+            Position p = new Position(1, 2);
+            Console.WriteLine(p);
+        }
+    }
+}
